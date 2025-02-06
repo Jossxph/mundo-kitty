94c4a74a0d3b5734542b0d945a6609fea1639a7b
Jossxph <jossxph@gmail.com>
2025-02-06 18:18:10 -0500

    V.1.0.3 Mejoras de Errores

--- src/js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('btn-ask').addEventListener('click', function() {
-    const message = document.getElementById('message');
-    message.classList.remove('hidden'); // Muestra el mensaje
-    this.textContent = '¡Gracias!'; // Cambia el texto del botón
-    this.disabled = true; // Desactiva el botón
-});
